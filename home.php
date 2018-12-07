ec8c73e8c63ed1c5659875d4debdaf568ec4bd8f
jayneeauza <33424432+jayneeauza@users.noreply.github.com>
2018-12-07 20:20:59 +0800

    Add files via upload

---
+++ home.php
@@ -0,0 +1,241 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+	<link rel="shortcut icon" type="image/x-icon" href="icons/ico-liceo.png"/>
+	<title>Liceo de Cagayan University</title>
+	<link rel="stylesheet" type="text/css" href="css/layout.css">
+	<link rel="stylesheet" type="text/css" href="css/home.css">
+</head>
+<body>
+	<div class="anmte-fadeIn">
+		<div  class="w3-row cldiv">
+			<div class="column left">
+				<a href="index.php"><img src="icons/ico-ldcu.png" width="340" height="80"></a>
+			</div>
+			<div id="divnav" class="column right w3-bar">
+				<!-- <ul id="idnav">
+					<li>ABOUT</li>
+					<li>ADMISSION</li>
+					<li>DEPARTMENT</li>
+					<li>RESEARCH & EXTENSION</li>
+					<li>NEWS & EVENTS</li>
+					<li>LICEAN</li>
+				</ul> -->
+				<div class="w3-dropdown-hover">
+					<button class="w3-button cl-navitem">ABOUT</button>
+					<div class="w3-dropdown-content w3-bar-block w3-border">
+						<a href="#" class="w3-bar-item w3-bar-block">HISTORY</a>					
+						<a href="#" class="w3-bar-item w3-bar-block">VISION & MISSION</a>					
+						<a href="#" class="w3-bar-item w3-bar-block">UNIVERSITY GOALS</a>					
+						<a href="#" class="w3-bar-item w3-bar-block">ADMINISTRATOR</a>
+						<a href="#" class="w3-bar-item w3-bar-block">RODELSA HALL</a>
+					</div>
+				</div>
+				<div class="w3-dropdown-hover">
+					<button class="w3-button cl-navitem">ADMISSION</button>
+					<div class="w3-dropdown-content w3-bar-block w3-border">
+						<a href="#" class="w3-bar-item w3-bar-block">SCHOLARSHIP</a>					
+						<a href="#" class="w3-bar-item w3-bar-block">ENROLLMENT</a>					
+					</div>
+				</div>
+				<div class="w3-dropdown-hover">
+					<button class="w3-button cl-navitem">DEPARTMENT</button>
+					<div class="w3-dropdown-content w3-bar-block w3-border">
+						<a href="#" class="w3-bar-item w3-bar-block">BASIC EDUCATION</a>
+						<a href="#" class="w3-bar-item w3-bar-block">UNDERGRADUATE</a>
+						<a href="#" class="w3-bar-item w3-bar-block">GRADUATE</a>
+						<a href="#" class="w3-bar-item w3-bar-block">FOREIGN STUDENTS</a>
+					</div>
+				</div>
+				<div class="w3-dropdown-hover">
+					<button class="w3-button cl-navitem">RESEARCH & EXTENSION</button>
+					<div class="w3-dropdown-content w3-bar-block">
+						<a href="#" class="w3-bar-item w3-bar-block w3-border">VISION & MISSION</a>					
+						<a href="#" class="w3-bar-item w3-bar-block w3-border">RESEARCH IMPACT</a>					
+						<a href="#" class="w3-bar-item w3-bar-block w3-border">LINKAGES</a>					
+					</div>
+				</div>
+				<div class="w3-dropdown-hover">
+					<button class="w3-button cl-navitem">NEWS & EVENTS</button>
+					<div class="w3-dropdown-content w3-bar-block w3-border">
+						<a href="#" class="w3-bar-item w3-bar-block w3-border">INCOMING EVENTS</a>					
+						<a href="#" class="w3-bar-item w3-bar-block w3-border">NEWS RELEASES</a>					
+						<a href="#" class="w3-bar-item w3-bar-block w3-border">BLOGS</a>					
+					</div>
+				</div>
+				<div class="w3-dropdown-hover">
+					<button class="w3-button cl-navitem">LICEAN</button>
+					<div class="w3-dropdown-content w3-bar-block w3-border">
+						<a href="#" class="w3-bar-item w3-bar-block">THE LICEAN</a>					
+						<a href="#" class="w3-bar-item w3-bar-block">ORGANIZATIONS</a>					
+						<a href="#" class="w3-bar-item w3-bar-block">STUDENT PERSONNEL SERVICES</a>					
+						<a href="#" class="w3-bar-item w3-bar-block">GUIDANCE AND PLACEMENT</a>					
+					</div>
+				</div>
+			</div>
+		</div>
+		<div class="w3-row cldiv"> <!-- BANNER SLIDE -->
+			<div id="divslide" class="w3-container ">
+				<div class="mySlides w3-container">
+					<h1>Sample slide</h1>
+				</div>
+				<div class="mySlides w3-container">
+					<h1>Sample slide2</h1>
+				</div>
+				<div class="w3-center w3-container w3-section w3-large w3-text-white" style="width:100%">
+					<div class="w3-left w3-hover-text-khaki" onclick="plusDivs(-1)">&#10094;</div>
+					<div class="w3-right w3-hover-text-khaki" onclick="plusDivs(1)">&#10095;</div>
+					<span class="w3-badge demo w3-border w3-transparent w3-hover-white" onclick="currentDiv(1)"></span>
+				    <span class="w3-badge demo w3-border w3-transparent w3-hover-white" onclick="currentDiv(2)"></span>
+				    <span class="w3-badge demo w3-border w3-transparent w3-hover-white" onclick="currentDiv(3)"></span>
+				</div>
+			</div>
+		</div>
+		<div class="w3-row cldiv"> <!-- ABOUT LICEO -->
+			<div id="divabt" class="w3-container cl-pad-2">
+				<h3 class="cl-txt-cntr">Liceo de Cagayan University is a prime private, non-sectarian university in Northern Mindanao that prepares students for productive life in the society through an offering of a wide array of programs and activities that facilitate Total Human Formation.</h3>
+			</div>
+			<!-- <div class="cl-rlt">
+				<div class="column">sample1</div>
+				<div class="column">sample2</div>
+				<div class="column">sample3</div>
+			</div> -->
+			<div class="cl-rlt">
+				<table border="1">
+					<tr>
+						<td>img_1</td>
+						<td>img_2</td>
+						<td>img_3</td>
+					</tr>
+					<tr>
+						<td>1td</td>
+						<td>2td</td>
+						<td>3td</td>
+					</tr>
+				</table>
+			</div>
+		</div>
+		<div class="w3-row"> <!-- FEATURED NEWS -->
+			<h3 class="cl-head">FEATURED NEWS</h3>
+			<div class="column cldiv">
+				<div class="w3-row pad-1">
+					<h5>FDT EUROPE TOUR 2018</h5>
+					<p>After the unparalleled success of their one-month Folklore tour in Europe, the Liceo Folkloric Dance Troupe will stage a HOMECOMING CONCERT on September 18 at the Rodelsa Hall.The Liceo FDT was the sole Asian Dance group invited to participate in seven international folklore festivals in France, Bulgaria, and Romania from June 29 to August 2 of this year. The first festival, the 35th Festival des Cultures du Monde was held in Voiron, France by the Arts & Popular Traditions Samorenc Voiron-Chartreuse, a French Folklore Dance Group whose Director is a founding member of the WORLD FOLKLORE UNION I.G.F.(IGF).  In Voiron, the FDT joined groups from Madagascar, Macedonia, Paraguay and Israel-Palestine, and Canada in a week-long festival sharing cultures through daily performances and street animations.</p>
+				</div>
+				<div class="w3-row">
+					<img src="">
+				</div>
+			</div>
+			<div class="column cldiv" style="border:1px solid red;">
+				<div class="w3-row">
+					<img src="">
+				</div>
+				<div class="w3-row pad-1">
+					<h5>HIV/AIDS Awareness and Substance AbuseEducation Seminar</h5>
+					<p>October 3, 2018 - The NSTP class of the Medical Laboratory Science - Block D, with their instructor, Mr. Kimberly B. Alvarez, organized and hosted a seminar on HIV/AIDS Awareness and Substance Abuse Education held at AVR 3 in this university.  It was associated with its point of engagement- what an individual can do to avoid the said issues.Mrs. Stella Ferrarez, a Psychologist and an employee of Philippine Mental Health Association was the guest speaker of the event. Mrs. Ferrarez emphasized on the spread of HIV/AIDS to the youngsters, its implications to one’s health and morality, and most specifically how this can be evaded. Her experience in the profession opened her mind to become an advocate.</p>
+				</div>
+			</div>
+		</div>
+		<div id="divuninews" class="w3-row"> <!-- UNIVERSITY NEWS -->
+			<h3 class="cl-head">UNIVERSITY NEWS</h3>
+			<div id="div-grid-news" class="w3-container">
+				<div class="column">
+					<img src="images/rodelsahall.jpg" width="100%" height="200"><br>
+					<span class="cl-cap-ttle">Buwan ng Wika Liceo Preschool</span>
+					<p class="cl-cap">Ipinagdiwang ng Liceo Preschool ang Buwan ng Wika 2018 sa pamamagitan ng “Tabo sa Priskol” noong nakaraang Agosto 31, 2018.</p>
+				</div>
+				<div class="column">
+					<img src="images/rodelsahall.jpg" width="100%" height="200"><br>
+					<span class="cl-cap-ttle">Buwan ng Wika Liceo Preschool</span>
+					<p class="cl-cap">Ipinagdiwang ng Liceo Preschool ang Buwan ng Wika 2018 sa pamamagitan ng “Tabo sa Priskol” noong nakaraang Agosto 31, 2018.</p>
+				</div>
+				<div class="column">
+					<img src="images/rodelsahall.jpg" width="100%" height="200"><br>
+					<span class="cl-cap-ttle">Buwan ng Wika Liceo Preschool</span>
+					<p class="cl-cap">Ipinagdiwang ng Liceo Preschool ang Buwan ng Wika 2018 sa pamamagitan ng “Tabo sa Priskol” noong nakaraang Agosto 31, 2018.</p>
+				</div>
+				<div class="column">
+					<img src="images/rodelsahall.jpg" width="100%" height="200"><br>
+					<span class="cl-cap-ttle">Buwan ng Wika Liceo Preschool</span>
+					<p class="cl-cap">Ipinagdiwang ng Liceo Preschool ang Buwan ng Wika 2018 sa pamamagitan ng “Tabo sa Priskol” noong nakaraang Agosto 31, 2018.</p>
+				</div>
+			</div>
+		</div>
+		<div id="divftlinks" class="w3-row"> <!-- FOOTER -->
+			<div class="clmn-1 column">
+				<ul class="n-list">
+					<li>ABOUT</li>
+					<li>SAMPLE</li>
+					<li>SAMPLE</li>
+					<li>SAMPLE</li>
+				</ul>
+			</div>
+			<div id="divcntctinfo" class="clmn-2 column">
+				<div class="column">
+					<span>Subscribe to our newsletter</span><br>
+					<input type="text" name="">
+					<button class="w3-button w3-white">OK</button>
+				</div>
+				<div class="column">
+					<span>RNP Blvd., Kauswagan, Cagayan de Oro City</span><br>
+					<span>+88 858 4093</span><br>
+					<span>liceo@liceo.edu.ph</span>
+				</div>
+			</div>
+		</div>
+		<div id="divftcpyrght" class="w3-row">
+			<div class="column">
+				<p>© Liceo de Cagayan University. 2018.</p>
+			</div>
+			<div class="column">
+				<img src="">
+			</div>
+		</div>
+	</div>
+<script type="text/javascript">
+	var slideIndex = 1;
+
+	carousel(currentDiv());
+
+	function carousel() {
+		var i;
+		var x = document.getElementsByClassName("mySlides");
+		for (i = 0; i < x.length; i++) {
+			x[i].style.display = "none";
+		}
+		slideIndex++;
+		if (slideIndex > x.length) {
+			slideIndex = 1
+		}
+		x[slideIndex-1].style.display = "block";
+		setTimeout(carousel, 2000);
+	}
+
+	function plusDivs(n) {
+		showDivs(slideIndex += n);
+	}
+
+	function currentDiv(n) {
+		showDivs(slideIndex = n);
+	}
+
+	function showDivs(n) {
+		var i;
+		var x = document.getElementsByClassName("mySlides");
+		var dots = document.getElementsByClassName("demo");
+		if (n > x.length) {slideIndex = 1}    
+		if (n < 1) {slideIndex = x.length}
+		for (i = 0; i < x.length; i++) {
+		    x[i].style.display = "none";  
+		}
+		for (i = 0; i < dots.length; i++) {
+		    dots[i].className = dots[i].className.replace(" w3-white", "");
+		}
+		x[slideIndex-1].style.display = "block";  
+		dots[slideIndex-1].className += " w3-white";
+	}
+</script>
+</body>
+</html>
---
+++ js/app.js
@@ -0,0 +1,9 @@
+$(document).ready(function() {
+	$("#btnhome").click(function() {
+		window.location.href="home.php";
+	});
+
+	$("#idadown").click(function() {
+		window.location.href="home.php";
+	});
+});
