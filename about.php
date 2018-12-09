e353eb07a66671eae50aa914e61853fb9f83c43b
jayneeauza <33424432+jayneeauza@users.noreply.github.com>
2018-12-09 21:00:11 +0800

    Create about.php

---
+++ about.php
@@ -0,0 +1,47 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+	<link rel="shortcut icon" type="image/x-icon" href="icons/ico-liceo.png"/>
+	<title>Liceo de Cagayan University</title>
+	<link rel="stylesheet" type="text/css" href="css/layout.css">
+	<link rel="stylesheet" type="text/css" href="css/about.css">
+</head>
+<body>
+	<div class="divbody">
+		<div class="w3-container w3-row">
+			<div class="column left">
+				<a href="index.php"><img src="icons/ico-ldcu.png" width="340" height="80"></a>
+			</div>
+			<div class="column right nav">
+				<ul>
+					<li><a href="">HOME</a></li>
+					<li><a href="">VISION & MISSION</a></li>
+					<li><a href="">GOALS</a></li>
+					<li><a href="">ADMINISTRATOR</a></li>
+					<li><a href="">RODELSA HALL</a></li>
+				</ul>
+			</div>
+		</div>
+		<div class="w3-row">
+			<div class="w3-row w3-border">
+				<img src="images/sample.png" width="100%" height="600">
+				<div id="txt-history" class="txt w3-container w3-border w3-display-right">
+					<h3>HISTORY</h3>
+					<p>Founded in 1955 by Atty. Rodolfo N. Pelaez of Cagayan de Oro City and his wife Elsa Pelaez of Cebu Cify, the University was established with the primary vision of extending education to the youths especially those poor but deserving students in Northern Mindanao. Envisioned to make relevant and quality education accessible to the youths, this higher institution of learning was founded to facilitate their total human formation anchored on moral values and Christian precepts.</p>
+				</div>
+			</div>
+			<div class="w3-row w3-border hdn">
+				<img src="images/sample.png" width="100%" height="600">
+				<div id="txt-admin" class="txt w3-container w3-row w3-border w3-display-left">
+					<h3>THE ADMINISTRATOR</h3>
+					<p>Liceo de Cagayan University, as a close family corporation, is headed by a president who is appointed by a board of directors. It has a flat and lean administrative structure, making governance and management responsive, effective, and action-oriented.</p>
+					<p>The University has stood the test of time through a strong, dynamic, and competent leadership at the helm. Upholding the philanthropic vision to serve the educational need of the common man, Atty. Rodolfo N. Pelaez and his wife Elsa P. Pelaez, founded this post-liberation institution in 1955. Succeeding Atty. Rodolfo N. Pelaez, who served as Director of Liceo de Cagayan, Dr. Jose Ma. R. Golez was installed as the first University President in 1997, with a five-year term of office. In 2002, Dr. Rafaelita P. Pelaez, was installed as the second University President and continued to pursue tirelessly the ideals and aspirations of the University’s reason for being. After five years, Dr. Mariano M. Lerin, CPA, was installed in 2007 as the third University president. He is the concurrent University President as of this writing.</p>
+					<p>After 59 years, Liceo de Cagayan University continues to commit as a Filipino institution of higher learning, the University is committed to the upholding of national identity by instilling in the heart of every student the pride as a Filipino. The University continues to stand for excellence, integrity, loyalty, discipline and service as central to the total human formation of students.</p>
+				</div>
+			</div>
+		</div>
+	</div>
+</body>
+</html>
