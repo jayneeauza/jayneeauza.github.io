b7c0569371ea6a7b030d17c64bbe1ddc6aac6964
jayneeauza <33424432+jayneeauza@users.noreply.github.com>
2020-03-06 13:47:21 +0800

    Delete app.js

--- js/app.js
+++
@@ -1,9 +0,0 @@
-$(document).ready(function() {
-	$("#btnhome").click(function() {
-		window.location.href="home.html";
-	});
-
-	$("#idadown").click(function() {
-		window.location.href="home.html";
-	});
-});
